49cfa95cadb2b02459c1d14ce2f115ebd83c8d9a
魔才 <jarlene.xwl@alibaba-inc.com>
2018-03-01 17:08:11 +0800

    add test

--- ui/user.lua
+++ ui/user.lua
@@ -6,3 +6,15 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+local User = {}
+local returncode = require "conf.returncode"
+local function test()
+    local res = { code = returncode.SUCCESS.code, msg = returncode.SUCCESS.msg }
+    return res
+end
+
+
+
+
+User.test = test
+return User
